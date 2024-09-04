c84f619f79e231daa72d86d56a8e46ede19f70d8
Ebenezer Ndudim Nmesoma <ebenezerndudim10@gmail.com>
2024-09-04 13:29:12 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+/////EBENEZER'S WORK 9/4/2024
+
+//Question 1
+//Create a Variable called K and assign the value 42 to it and comment it's data type
+let K = 42;
+//The data type of K is a number(Primitive Data Type)
+
+
+//Question 2
+//Create a Variable called Y, assign 3.142 to it and  comment it's data type
+let y = 3.142;
+//The data type of y is a number
+
+
+//Question 3
+//Create a Variable called car name and assign the value "Bear" to it
+let carName = "Benz";
+//this data type is a string
+
+//Question 4
+//Display the sum of 6+13 using two variable a and b
+let a = 6;
+let b = 13;
+
+let sum = a + b;
+console.log(sum); //output: 19
+
+//Question 5
+//Multiply 15 and 5, alert the result
+let result = 15 * 5;
+
+//ALert for the pop up
+alert(result);
+
+
+//Question 6
+//Divide 12 by 3, alert the result
+let result = 12 / 3;
+
+alert(result);
+
+//Question 7
+//Alert the remainder when 17 is divided by 3
+let remainder = 17 % 3;
+
+alert(remainder);
+
+
+//Question 8
+//Use the Correct assignment operator that will result x being 15
+let x = 15;
+//Direct Assignment Operator
+
+//Question 9
+//USe comments to describe the correct data types of a 15 b your name c 16.75 d false
+
+//a. 15
+// The data type of 15 is a number (Integer)
+
+//b. "Your Name"
+// The data type of "Your Name" is a string
+
+//c. 16.75
+// The data type of 16.75 is a number
+
+//d. false
+// The data type of false is a boolean
